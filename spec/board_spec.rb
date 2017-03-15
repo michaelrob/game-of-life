714d5ff993aa18defed15d7581ca91ffef1551b9
Michael Robinson <seraph890@gmail.com>
2017-03-15 12:33:27 +0900

    Added basic specs and stubbed out tests

---
+++ spec/board_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'board'
+
+describe Board do
+  let(:board) { Board.new(80, 20) }
+
+  it 'should generate board' do
+    binding.pry
+  end
+
+  it 'should be 80 wide and 20 high' do
+  end
+
+  it 'should generate board with live cell' do
+  end
+end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'cell'
+
+describe Cell do
+
+  describe '#neighbours' do
+    it 'should return neighbouring cells' do
+
+    end
+  end
+end
