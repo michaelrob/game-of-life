d25437211d3deab73fd792e6de55463b6ef7f818
Michael Robinson <seraph890@gmail.com>
2017-03-17 10:22:47 +0900

    cleaned up spec, removed un required specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'board'
 
 describe Board do
-  let(:board) { Board.new(10, 10) }
+  let(:board) { Board.new(10, 10, [[2,2], [2,3], [1,2]]) }
 
   it 'should generate board' do
   end
@@ -13,23 +13,8 @@ describe Board do
   it 'should generate board with cell' do
   end
 
-  describe '#cells' do
-    it 'should flatten cells' do
-      expect(board.cells.length).to be 45
-    end
-  end
-
   describe '#tick' do
     it 'should do the things' do
-      cell = board.get_cell(2, 2)
-      cell.alive!
-
-      cell_at_left = board.get_cell(2, 3)
-      cell_at_left.alive!
-
-      cell_at_right = board.get_cell(1, 2)
-      cell_at_right.alive!
-
       world = board.tick!
       binding.pry
     end
