4c1eb8ff227e57abecd50644886de1005db9c3dd
Michael Robinson <seraph890@gmail.com>
2017-03-17 20:32:03 +0900

    added lots of empty specs

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,10 +2,57 @@ require 'spec_helper'
 require 'cell'
 
 describe Cell do
+  let(:board) { Board.new }
+  let(:cell) { Cell.new(board, 0, 0) }
+
+  describe '#dead?' do
+    contex 'cell is dead' do
+      it 'should return true' do
+        #
+      end
+    end
+
+    context 'cell is alive' do
+      it 'should return false' do
+        #
+      end
+    end
+  end
+
+  describe '#alive?' do
+    context 'cell is dead' do
+      it 'should return false' do
+        #
+      end
+    end
+
+    context 'cell is alive' do
+      it 'should return true' do
+        #
+      end
+    end
+  end
+
+  describe '#alive!' do
+    it 'should change cell alive to true' do
+      #
+    end
+  end
+
+  describe '#switch!' do
+    context 'cell is dead' do
+      it 'should change cell alive to false' do
+        #
+      end
+    end
+
+    context 'cell is alive' do
+      it 'should change cell alive to true' do
+        #
+      end
+    end
+  end
 
   describe '#neighbours' do
-    it 'should return neighbouring cells' do
-
-    end
   end
 end
