f06ab27b3e83dd42bb8c1780840ee1fcea5060f7
Michael Robinson <seraph890@gmail.com>
2017-03-17 11:51:58 +0900

    updated board spec with stubbed out tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'board'
 
 describe Board do
-  let(:board) { Board.new(10, 10, [[2,2], [2,3], [1,2]]) }
+  let(:board) { Board.new(10, 10) }
 
   it 'should generate board' do
   end
@@ -14,9 +14,20 @@ describe Board do
   end
 
   describe '#tick' do
-    it 'should do the things' do
-      world = board.tick!
-      binding.pry
+    context 'cell is live' do
+      it 'should return dead cell if fiewer than two neighbours are live' do
+      end
+
+      it 'should return live cell if two or three neighbours are live' do
+      end
+
+      it 'should return dead cell if more than three neighbours are live' do
+      end
+    end
+
+    context 'cell is dead' do
+      it 'should return live cell if three neighbours are live' do
+      end
     end
   end
 end
