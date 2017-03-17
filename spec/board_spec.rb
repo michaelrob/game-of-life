50ec6c384033475ec0a32c0ceb990945c8064cbe
Michael Robinson <seraph890@gmail.com>
2017-03-17 14:07:09 +0900

    upated specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 require 'board'
 
 describe Board do
-  let(:board) { Board.new(10, 10) }
+  let(:board) { Board.new(10, 10, [[0,0], [0,1], [0,2]]) }
+  let(:board_two) { Board.new(10, 10, [[0,0], [0,1], [0,2], [1,1], [1,0]]) }
+  let(:board_three) { Board.new(10, 10, [[0,0], [0,1], [0,2], [1,1], [1,0]]) }
+  let(:board_four) { Board.new(10, 10, [[0,0], [0,1], [0,2]]) }
 
   it 'should generate board' do
   end
@@ -16,12 +19,21 @@ describe Board do
   describe '#tick' do
     context 'cell is live' do
       it 'should return dead cell if fiewer than two neighbours are live' do
-      end
+        board.tick!
 
-      it 'should return live cell if two or three neighbours are live' do
+        expect(board.cells[0,0].alive?).to be false
       end
 
       it 'should return dead cell if more than three neighbours are live' do
+        board_two.tick!
+
+        expect(board_two.cells[1,1].alive?).to be false
+      end
+
+      it 'should return live cell if two or three neighbours are live' do
+        board_three.tick!
+
+        expect(board_two.cells[0,1].alive?).to be true
       end
     end
 
